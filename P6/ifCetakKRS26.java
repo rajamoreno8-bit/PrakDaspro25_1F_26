cdbf16b4d0f74dfa833cbbf1c2ea06733d969e97
rajamoreno8 <rajamoreno8@gmail.com>
2025-10-09 11:14:36 +0700

    Tugas 1

--- P6/ifCetakKRS26.java
+++ P6/ifCetakKRS26.java
@@ -3,17 +3,22 @@ package P6;
 import java.util.Scanner;
 
 public class ifCetakKRS26 {
-    
+
     public static void main(String[] args) {
         Scanner Raja26 = new Scanner(System.in);
+
         System.out.println("--- Cetak KRS SIAKAD ---");
-        System.out.print("Apakah UKT sudah lunas? (true/false) :");
+        System.out.print("Apakah UKT sudah lunas? (true/false) : ");
         boolean uktLunas = Raja26.nextBoolean();
 
-    if (uktLunas) {
-        System.out.println("Pembayaran UKT terverivikasi");
-        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-    }else{
-        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        // Struktur if-else
+        if (uktLunas == true) {
+            System.out.println("Pembayaran UKT terverifikasi.");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA.");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
+        }
+
+        Raja26.close(); // menutup scanner agar tidak ada memory leak
     }
-    }}
+}
