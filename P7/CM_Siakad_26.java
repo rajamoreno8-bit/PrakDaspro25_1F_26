aa3165a638c011a1f1f387e3cbb45d0556a512b5
rajamoreno8 <rajamoreno8@gmail.com>
2025-10-16 07:55:44 +0700

    CM_Siakad_26

--- P7/CM_Siakad_26.java
+++ P7/CM_Siakad_26.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class CM_Siakad_26 {
 
     public static void main(String[] args) {
-         Scanner Raja26 = new Scanner(System.in);
-        String nama, nim;
-        int Pancasila, Daspro, Bing;
-        int skspancasila = 2;
-        int sksdaspro = 3;
-        int sksbing = 2;
+        
+        Scanner Raja26 = new Scanner(System.in);
+        
+         String nama, nim;
+        int Pancasila26, Daspro26, Bing26;
+        int skspancasila26 = 2;
+        int sksdaspro26 = 3;
+        int sksbing26 = 2;
 
         System.out.println("===== INPUT MAHASISWA =====");
         System.out.print("Nama : ");
@@ -21,116 +23,116 @@ public class CM_Siakad_26 {
 
         System.out.println("Masukan Nilai Angka (0 - 100): ");
         System.out.print("Pancasila : ");
-        Pancasila = Raja26.nextInt();
+        Pancasila26 = Raja26.nextInt();
         System.out.print("Dasar Pemorgramman : ");
-        Daspro = Raja26.nextInt();
+        Daspro26 = Raja26.nextInt();
         System.out.print("Bahasa Inggris : ");
-        Bing = Raja26.nextInt();
+        Bing26 = Raja26.nextInt();
 
-        if (Pancasila < 0 || Pancasila > 100) {
+        if (Pancasila26 < 0 || Pancasila26 > 100) {
             System.out.println("[ERROR] Nilai tidak valid! Semua nilai harus berada di antara 0 - 100");
-        } else if(Daspro < 0 || Daspro > 100){
+        } else if(Daspro26 < 0 || Daspro26 > 100){
             System.out.println("[ERROR] Nilai tidak valid! Semua nilai harus berada di antara 0 - 100");
-        } else if(Bing < 0 || Bing > 100){
+        } else if(Bing26 < 0 || Bing26 > 100){
             System.out.println("[ERROR] Nilai tidak valid! Semua nilai harus berada di antara 0 - 100");
         }
 
-        String nilaiAkhirP, nilaiAkhirD, nilaiAkhirB;
-        if (Pancasila > 80 && Pancasila <= 100){    
-            nilaiAkhirP = "A";
-        }else if(Pancasila > 73 && Pancasila <=80){
-            nilaiAkhirP = "B+";
-        }else if(Pancasila > 65 && Pancasila <= 73){
-            nilaiAkhirP = "B";
-        }else if(Pancasila > 60 && Pancasila <= 65){
-            nilaiAkhirP = "C+";
-        }else if(Pancasila > 50 && Pancasila <= 60){
-            nilaiAkhirP = "C";
-        }else if(Pancasila > 39 && Pancasila <= 50){
-            nilaiAkhirP = "D";
+        String nilaiAkhirP26, nilaiAkhirD26, nilaiAkhirB26;
+        if (Pancasila26 > 80 && Pancasila26 <= 100){    
+            nilaiAkhirP26 = "A";
+        }else if(Pancasila26 > 73 && Pancasila26 <=80){
+            nilaiAkhirP26 = "B+";
+        }else if(Pancasila26 > 65 && Pancasila26 <= 73){
+            nilaiAkhirP26 = "B";
+        }else if(Pancasila26 > 60 && Pancasila26 <= 65){
+            nilaiAkhirP26 = "C+";
+        }else if(Pancasila26 > 50 && Pancasila26 <= 60){
+            nilaiAkhirP26 = "C";
+        }else if(Pancasila26 > 39 && Pancasila26 <= 50){
+            nilaiAkhirP26 = "D";
         }else{
-            nilaiAkhirP = "E";
+            nilaiAkhirP26 = "E";
         }
 
-        if (Daspro > 80 && Daspro <= 100){    
-            nilaiAkhirD = "A";
-        }else if(Daspro > 73 && Daspro <=80){
-            nilaiAkhirD = "B+";
-        }else if(Daspro > 65 && Daspro <= 73){
-            nilaiAkhirD = "B";
-        }else if(Daspro > 60 && Daspro <= 65){
-            nilaiAkhirD = "C+";
-        }else if(Daspro > 50 && Daspro <= 60){
-            nilaiAkhirD = "C";
-        }else if(Daspro > 39 && Daspro <= 50){
-            nilaiAkhirD = "D";
+        if (Daspro26 > 80 && Daspro26 <= 100){    
+            nilaiAkhirD26 = "A";
+        }else if(Daspro26 > 73 && Daspro26 <=80){
+            nilaiAkhirD26 = "B+";
+        }else if(Daspro26 > 65 && Daspro26 <= 73){
+            nilaiAkhirD26 = "B";
+        }else if(Daspro26 > 60 && Daspro26 <= 65){
+            nilaiAkhirD26 = "C+";
+        }else if(Daspro26 > 50 && Daspro26 <= 60){
+            nilaiAkhirD26 = "C";
+        }else if(Daspro26 > 39 && Daspro26 <= 50){
+            nilaiAkhirD26 = "D";
         }else{
-            nilaiAkhirD = "E";
+            nilaiAkhirD26 = "E";
         }
 
-        if (Bing > 80 && Bing <= 100){    
-            nilaiAkhirB = "A";
-        }else if(Bing > 73 && Bing <=80){
-            nilaiAkhirB = "B+";
-        }else if(Bing > 65 && Bing <= 73){
-            nilaiAkhirB = "B";
-        }else if(Bing > 60 && Bing <= 65){
-            nilaiAkhirB = "C+";
-        }else if(Bing > 50 && Bing <= 60){
-            nilaiAkhirB = "C";
-        }else if(Bing > 39 && Bing <= 50){
-            nilaiAkhirB = "D";
+        if (Bing26 > 80 && Bing26 <= 100){    
+            nilaiAkhirB26 = "A";
+        }else if(Bing26 > 73 && Bing26 <=80){
+            nilaiAkhirB26 = "B+";
+        }else if(Bing26 > 65 && Bing26 <= 73){
+            nilaiAkhirB26 = "B";
+        }else if(Bing26 > 60 && Bing26 <= 65){
+            nilaiAkhirB26 = "C+";
+        }else if(Bing26 > 50 && Bing26 <= 60){
+            nilaiAkhirB26 = "C";
+        }else if(Bing26 > 39 && Bing26 <= 50){
+            nilaiAkhirB26 = "D";
         }else{
-            nilaiAkhirB = "E";
+            nilaiAkhirB26 = "E";
         }
 
-        double nilaiSetaraP, nilaiSetaraD, nilaiSetaraB;
-        if(nilaiAkhirP.equals("A")){
-            nilaiSetaraP = 4;
-        }else if(nilaiAkhirP.equals("B+")){
-            nilaiSetaraP = 3.5;
-        }else if(nilaiAkhirP.equals("B")){
-            nilaiSetaraP = 3;
-        }else if(nilaiAkhirP.equals("C+")){
-            nilaiSetaraP = 2.5;
-        }else if(nilaiAkhirP.equals("C")){
-            nilaiSetaraP = 2;
-        }else if(nilaiAkhirP.equals("D")){
-            nilaiSetaraP = 1;
+        double nilaiSetaraP26, nilaiSetaraD26, nilaiSetaraB26;
+        if(nilaiAkhirP26.equals("A")){
+            nilaiSetaraP26 = 4;
+        }else if(nilaiAkhirP26.equals("B+")){
+            nilaiSetaraP26 = 3.5;
+        }else if(nilaiAkhirP26.equals("B")){
+            nilaiSetaraP26 = 3;
+        }else if(nilaiAkhirP26.equals("C+")){
+            nilaiSetaraP26 = 2.5;
+        }else if(nilaiAkhirP26.equals("C")){
+            nilaiSetaraP26 = 2;
+        }else if(nilaiAkhirP26.equals("D")){
+            nilaiSetaraP26 = 1;
         }else{
-            nilaiSetaraP = 0;
+            nilaiSetaraP26 = 0;
         }
 
-        if(nilaiAkhirD.equals("A")){
-            nilaiSetaraD = 4;
-        }else if(nilaiAkhirD.equals("B+")){
-            nilaiSetaraD = 3.5;
-        }else if(nilaiAkhirD.equals("B")){
-            nilaiSetaraD = 3;
-        }else if(nilaiAkhirD.equals("C+")){
-            nilaiSetaraD = 2.5;
-        }else if(nilaiAkhirD.equals("C")){
-            nilaiSetaraD = 2;
-        }else if(nilaiAkhirD.equals("D")){
-            nilaiSetaraD = 1;
+        if(nilaiAkhirD26.equals("A")){
+            nilaiSetaraD26 = 4;
+        }else if(nilaiAkhirD26.equals("B+")){
+            nilaiSetaraD26 = 3.5;
+        }else if(nilaiAkhirD26.equals("B")){
+            nilaiSetaraD26 = 3;
+        }else if(nilaiAkhirD26.equals("C+")){
+            nilaiSetaraD26 = 2.5;
+        }else if(nilaiAkhirD26.equals("C")){
+            nilaiSetaraD26 = 2;
+        }else if(nilaiAkhirD26.equals("D")){
+            nilaiSetaraD26 = 1;
         }else{
-            nilaiSetaraD = 0;
+            nilaiSetaraD26 = 0;
         }
 
-        if(nilaiAkhirB.equals("A")){
-            nilaiSetaraB = 4;
-        }else if(nilaiAkhirB.equals("B+")){
-            nilaiSetaraB = 3.5;
-        }else if(nilaiAkhirB.equals("B")){
-            nilaiSetaraB = 3;
-        }else if(nilaiAkhirB.equals("C+")){
-            nilaiSetaraB = 2.5;
-        }else if(nilaiAkhirB.equals("C")){
-            nilaiSetaraB = 2;
-        }else if(nilaiAkhirB.equals("D")){
-            nilaiSetaraB = 1;
+        if(nilaiAkhirB26.equals("A")){
+            nilaiSetaraB26 = 4;
+        }else if(nilaiAkhirB26.equals("B+")){
+            nilaiSetaraB26 = 3.5;
+        }else if(nilaiAkhirB26.equals("B")){
+            nilaiSetaraB26= 3;
+        }else if(nilaiAkhirB26.equals("C+")){
+            nilaiSetaraB26 = 2.5;
+        }else if(nilaiAkhirB26.equals("C")){
+            nilaiSetaraB26 = 2;
+        }else if(nilaiAkhirB26.equals("D")){
+            nilaiSetaraB26 = 1;
         }else{
-            nilaiSetaraB = 0;
+            nilaiSetaraB26 = 0;
         }
 
         System.out.println("==========================================================================");
@@ -142,19 +144,19 @@ public class CM_Siakad_26 {
         System.out.println("--------------------------------------------------------------------------");
         System.out.println("Mata Kuliah                SKS   Nilai   Huruf   Setara");
         System.out.println("--------------------------------------------------------------------------");
-        System.out.println("Pancasila                  2     " + Pancasila + "      " + nilaiAkhirP + "       " + nilaiSetaraP);
-        System.out.println("Dasar Pemrograman          3     " + Daspro + "      " + nilaiAkhirD + "       " + nilaiSetaraD);
-        System.out.println("Bahasa Inggris             2     " + Bing + "      " + nilaiAkhirB + "       " + nilaiSetaraB);
+        System.out.println("Pancasila                  2     " + Pancasila26 + "      " + nilaiAkhirP26 + "       " + nilaiSetaraP26);
+        System.out.println("Dasar Pemrograman          3     " + Daspro26 + "      " + nilaiAkhirD26 + "       " + nilaiSetaraD26);
+        System.out.println("Bahasa Inggris             2     " + Bing26 + "      " + nilaiAkhirB26 + "       " + nilaiSetaraB26);
         System.out.println("--------------------------------------------------------------------------");
 
         double totalSKS = 2 + 3 + 2;
-        double totalNilai = (nilaiSetaraP * skspancasila) + (nilaiSetaraD * sksdaspro) + (nilaiSetaraB * sksbing);
+        double totalNilai = (nilaiSetaraP26 * skspancasila26) + (nilaiSetaraD26 * sksdaspro26) + (nilaiSetaraB26 * sksbing26);
         double ip = totalNilai / totalSKS;
         double bobot = totalNilai;
 
         System.out.println("Total SKS : "+ totalSKS);
         System.out.println("Bobot : " + bobot);
-        System.out.println("IP");
+        System.out.println("IP ");
         System.out.println(String.format("%.2f",ip));
 
         String status;
@@ -164,7 +166,7 @@ public class CM_Siakad_26 {
             status = "Tidak Lulus";
         }
         System.out.println("Status Semester : " + status);
-        Raja26.close();
+        
 
         
 
