6a7e0991249ab7ec605984c750ecca47a680b0c6
rajamoreno8 <rajamoreno8@gmail.com>
2025-10-23 10:36:48 +0700

    P9_percobaan 1

---
+++ P9/SiakadFor26.java
@@ -0,0 +1,24 @@
+package P9;
+
+import java.util.Scanner;
+
+public class SiakadFor26 {
+    public static void main(String[] args) {
+        Scanner Raja26 = new Scanner(System.in);
+        double nilai, tertinggi = 0, terendah = 100;
+
+        for (int i = 1; i <= 10; i++){
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = Raja26.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        }
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+
+    }
+}
