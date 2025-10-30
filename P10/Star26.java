f939f01afd9ea30926b7bd5b2e0da8085a61eac2
rajamoreno8 <rajamoreno8@gmail.com>
2025-10-30 08:22:48 +0700

    Percobaan1_P10

---
+++ P10/Star26.java
@@ -0,0 +1,18 @@
+package P10;
+ 
+import java.util.Scanner;
+
+public class Star26 {
+    public static void main(String[] args) {
+         Scanner Raja26 = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai n = ");
+        int n = Raja26.nextInt();
+        for(int i=1; i<=n; i++){
+           System.out.print("*");
+        }
+
+
+    }
+    
+}
