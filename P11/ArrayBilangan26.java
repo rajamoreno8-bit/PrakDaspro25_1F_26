a7c70e4149aa24257c2136ce5cbc21ce6b47864a
rajamoreno8 <rajamoreno8@gmail.com>
2025-11-11 20:10:27 +0700

    P11_Percobaan1

---
+++ P11/ArrayBilangan26.java
@@ -0,0 +1,21 @@
+package P11;
+
+import java.util.Scanner;
+
+public class ArrayBilangan26 {
+    public static void main(String[] args) {
+        Scanner Akbar21 = new Scanner(System.in);
+
+        int [] bil = new int [4];
+
+        bil [0] = 5;
+        bil [1] = 13;
+        bil [2] = -7;
+        bil [3] = 17;
+
+        System.out.println(bil [0]);
+        System.out.println(bil [1]);
+        System.out.println(bil [2]);
+        System.out.println(bil [3]);
+    }
+}
