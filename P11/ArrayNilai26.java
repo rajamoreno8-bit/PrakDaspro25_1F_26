e641a975f700a3510efa3fab93946d235facc6ee
rajamoreno8 <rajamoreno8@gmail.com>
2025-11-11 20:11:04 +0700

    P11_Percobaan2

---
+++ P11/ArrayNilai26.java
@@ -0,0 +1,20 @@
+package P11;
+
+import java.util.Scanner;
+
+public class ArrayNilai26 {
+    public static void main(String[] args) {
+        Scanner Akbar21 = new Scanner(System.in);
+
+        int [ ] nilaiAkhir = new int[10];
+
+        for (int i =0; i< 10; i++) {
+            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
+            nilaiAkhir[i] = Akbar21.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-"+i+"adalah "+nilaiAkhir[i]);
+        }
+    }
+}
