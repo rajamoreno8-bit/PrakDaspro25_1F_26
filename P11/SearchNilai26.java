1c775b94d9b8495bf6a156ed09e38e5baf1c12d6
rajamoreno8 <rajamoreno8@gmail.com>
2025-11-11 20:12:29 +0700

    P11_Percobaan4

---
+++ P11/SearchNilai26.java
@@ -0,0 +1,23 @@
+package P11;
+
+import java.util.Scanner;
+
+public class SearchNilai26 {
+    public static void main(String[] args) {
+        Scanner Akbar21 = new Scanner(System.in);
+
+        int [] arrNilai = {80, 85, 79, 96, 90, 82, 86};
+        int key = 90;
+        int hasil = 0;
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (key == arrNilai[i]) {
+                hasil = i;
+                break;
+            }
+        }
+        System.out.println();
+        System.out.println("Nilai "+key+" Ketemu di indeks ke-"+hasil);
+        System.out.println();
+    }
+}
