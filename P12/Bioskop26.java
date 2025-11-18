d20ddd1729b40c45492f0ff32bae3ca23580b807
rajamoreno8 <rajamoreno8@gmail.com>
2025-11-18 20:49:46 +0700

    P12_Pertanyaan5_Percobaan1

--- P12/Bioskop26.java
+++ P12/Bioskop26.java
@@ -13,16 +13,18 @@ public class Bioskop26 {
         penonton [2][1] = "Farhan";
         penonton [3][0] = "Gisel";
         penonton [3][1] = "Hana";
+
+        System.out.println(penonton.length);
+
+        for (int i = 0; i < penonton.length; i++){
+            System.out.println("Panjang Baris Ke - " + (i+1) + ":" + penonton[i].length);
+        }
         
         System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
 
-        System.out.println(penonton.length);
-        System.out.println(penonton[0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton[2].length);
-        System.out.println(penonton[3].length);
+        
     }
 }
