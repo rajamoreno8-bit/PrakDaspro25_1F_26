919b56494078edba1f36b3e68a0d8e2a32653816
rajamoreno8 <rajamoreno8@gmail.com>
2025-12-03 19:05:00 +0700

    P14_Percobaan2

--- P14/Kafe26.java
+++ P14/Kafe26.java
@@ -1,6 +1,16 @@
 public class Kafe26 {
 
     public static void main(String[] args) {
+        Menu("Andi", true);
+    }
+
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat Datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda Adalah Member, Dapatkan Diskon 10% Untuk Setiap Pembelian");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappucino - Rp 20,000");
@@ -8,9 +18,7 @@ public class Kafe26 {
         System.out.println("4. Teh Tarik - Rp 12,000");
         System.out.println("5. Roti Bakar - Rp 10,000");
         System.out.println("6. Mie Goreng - Rp 18,000");
-        System.out.println("==========================");
-        System.out.println("Silahkan Pilih Menu Yang anda Inginkan");
+        System.out.println("===========================");
+        System.out.println("Silahkan Pilih Menu Yang Anda Inginkan.");
     }
-
-    
 }
