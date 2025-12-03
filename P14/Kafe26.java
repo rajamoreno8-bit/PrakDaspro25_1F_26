15a963e65b29ab50bd0a0fe2224c59e5e062b754
rajamoreno8 <rajamoreno8@gmail.com>
2025-12-03 19:23:42 +0700

    P14_Percobaan3

--- P14/Kafe26.java
+++ P14/Kafe26.java
@@ -1,14 +1,27 @@
+
+
+import java.util.Scanner;
+
 public class Kafe26 {
 
     public static void main(String[] args) {
+        Scanner Raja26 = new Scanner(System.in);
 
         Menu("Andi", true, "DISKON 50%");
-        Menu("Budi", true, "DISKON 30%");
+
+        System.out.print("\nMasukkan Nomor Menu Yang Ingin Anda Pesan: ");
+        int pilihanMenu = Raja26.nextInt();
+
+        System.out.print("Masukkan Jumlah Item Yang Ingin Di Pesan: ");
+        int banyakItem =Raja26.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total Harga Untuk Pesanan Anda: Rp " + totalHarga);
     }
 
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
-
-        System.out.println("Selamat Datang, " + namaPelanggan + "!");
+        System.out.println("Selamat Datang , " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda Adalah Member, Dapatkan Diskon 10% Untuk Setiap Pembelian");
@@ -23,14 +36,20 @@ public class Kafe26 {
         }
 
         System.out.println("===== MENU RESTO KAFE =====");
-        System.out.println("1. Kopi Hitam  - Rp 15.000");
-        System.out.println("2. Cappucino   - Rp 20.000");
-        System.out.println("3. Latte       - Rp 22.000");
-        System.out.println("4. Teh Tarik   - Rp 12.000");
-        System.out.println("5. Roti Bakar  - Rp 10.000");
-        System.out.println("6. Mie Goreng  - Rp 18.000");
+        System.out.println("1. Kopi Hitam  - Rp 15,000");
+        System.out.println("2. Cappucino   - Rp 20,000");
+        System.out.println("3. Latte       - Rp 22,000");
+        System.out.println("4. Teh Tarik   - Rp 12,000");
+        System.out.println("5. Roti Bakar  - Rp 10,000");
+        System.out.println("6. Mie Goreng  - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silahkan Pilih Menu Yang Anda Inginkan.");
-        System.out.println();
+    }
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
     }
 }
