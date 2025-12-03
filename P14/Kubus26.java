a7b412b0ac9b9bf3eff0eb74ef7174261c6603de
rajamoreno8 <rajamoreno8@gmail.com>
2025-12-03 19:47:31 +0700

    P14_Tugas1

---
+++ P14/Kubus26.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class KubusNoAbsen26 {
+
+    public double hitungVolume(double sisi) {
+        return sisi * sisi * sisi;
+    }
+
+    public double hitungLuasPermukaan(double sisi) {
+        return 6 * sisi * sisi;
+    }
+}
